ca8b20fb818bc7ca25aded78443ba0b145f72c33
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-08-24 18:57:36 -0300

    feat: criando componente de product e carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,57 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
+import Image from 'next/image'
+import camisa1 from '../assets/camisetas/1.png'
+import camisa2 from '../assets/camisetas/2.png'
+import camisa3 from '../assets/camisetas/3.png'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: "bold"
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <Button>
-      <span>teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camisa1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
