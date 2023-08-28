9a26093f0cb5a3a7215e7cf1597ad47d1ad608e2
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-08-28 18:58:13 -0300

    Navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { HomeContainer, Product } from "../styles/pages/home"
 import Image from 'next/image'
-
+import Link from 'next/link'
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
@@ -29,14 +29,16 @@ export default function Home({ products }: HomeProps) {
       {
         products.map((product) => {
           return (
-            <Product key={product.id} className="keen-slider__slide" >
-              <Image src={product.imageUrl} alt="" width={520} height={480} />
+            <Link key={product.id} href={`/product/${product.id}`}>
+              <Product className="keen-slider__slide" >
+                <Image src={product.imageUrl} alt="" width={520} height={480} />
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           )
         })
       }
