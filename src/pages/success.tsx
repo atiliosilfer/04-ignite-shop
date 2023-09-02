e84a8db7c1b4ca206246da142bca81928b46b713
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-09-02 10:25:34 -0300

    Trabalhando com SEO

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -5,6 +5,7 @@ import { SuccessContainer } from "@/src/styles/pages/success";
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 interface SuccessProps {
   costumerName: string;
@@ -16,21 +17,28 @@ interface SuccessProps {
 
 export default function Success({ costumerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada!</h1>
+    <>
+      <Head>
+        <title>Compra efetuada | Ignite Shop</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <SuccessContainer>
+        <h1>Compra efetuada!</h1>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt="" />
-      </ImageContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageContainer>
 
-      <p>
-        Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-      </p>
-      <Link href="/">
-        Voltar ao catálogo
-      </Link>
+        <p>
+          Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+        </p>
+        <Link href="/">
+          Voltar ao catálogo
+        </Link>
 
-    </SuccessContainer>
+      </SuccessContainer>
+    </>
+
   )
 }
 
